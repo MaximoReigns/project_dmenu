d4f761487e29d6b4b8d5084ff7bd09689ab3ea63
Máximo Sánchez <id569723@outlook.com>
2021-07-01 22:19:29 -0500

    Se implementó botones para agregar al carrito y mejor performance en el código

--- src/app/mcdonald/mcdonald.page.ts
+++ src/app/mcdonald/mcdonald.page.ts
@@ -10,9 +10,50 @@ import { Router } from '@angular/router';
 })
 export class McdonaldPage {
 
+  items = ['Hamburguesa Normal', 'Hamburguesa Mc Pollo', 'Cajita Feliz', ];
+
+  normal = [
+    {
+      imglogo: "../../assets/static/logo.png",
+      imgLogoFood: "../../assets/static/logo macd.png",
+      category: "Comida",
+      nameHamburger: "Hamburguesa Normal",
+    }
+  ]
+
+  chicken = [
+    {
+      imgLogoFood: "../../assets/static/hamburgesa pollo.png",
+      nameHamburger: "Hamburguesa Mc Pollo",
+    }
+  ]
+
+  happy = [
+    {
+      imgLogoFood: "../../assets/static/cajita feliz.png",
+      nameHamburger: "Cajita Feliz",
+    }
+  ]
+
   constructor(private modalCtrl: ModalController, private router: Router ) { }
   ionViewDidEnter(){
     console.log('ionViewDidEnter DMENU');
+  }
+
+  addItemNormal(){
+    this.items.push('¡Hamburguesa Normal Agregada Exitosamente!');
+  }
+
+  addItemChicken(){
+    this.items.push('¡Hamburguesa Mc Pollo Agregada Exitosamente!');
+  }
+
+  addItemHappy(){
+    this.items.push('¡Cajita Feliz Agregada Exitosamente!');
+  }
+
+  deleteItems(index: number){
+    this.items.splice(index, 1);
   }
 
   async openModal(){
